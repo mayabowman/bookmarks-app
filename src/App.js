9419eed680b1ef24012a95cb23676a1f6387a2e0
Maya Bowman <mayashady1@gmail.com>
2019-10-31 14:49:22 -0400

    added EditBookmark component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import AddBookmark from './AddBookmark/AddBookmark'
+import EditBookmark from './EditBookmark/EditBookmark'
 import BookmarkList from './BookmarkList/BookmarkList'
 import BookmarksContext from './BookmarksContext'
 import Nav from './Nav/Nav'
 import config from './config'
-import Rating from './Rating/Rating'
 import './App.css'
-
-// const bookmarks = [
-  // {
-  //   id: 0,
-  //   title: 'Google',
-  //   url: 'http://www.google.com',
-  //   rating: '3',
-  //   desc: 'Internet-related services and products.'
-  // },
-  // {
-  //   id: 1,
-  //   title: 'Thinkful',
-  //   url: 'http://www.thinkful.com',
-  //   rating: '5',
-  //   desc: '1-on-1 learning to accelerate your way to a new high-growth tech career!'
-  // },
-  // {
-  //   id: 2,
-  //   title: 'Github',
-  //   url: 'http://www.github.com',
-  //   rating: '4',
-  //   desc: 'brings together the world\'s largest community of developers.'
-  // }
-// ];
 
 class App extends Component {
   state = {
@@ -70,12 +46,24 @@ class App extends Component {
     })
       .then(res => {
         if (!res.ok) {
-          throw new Error(res.status)
+          return res.json().then(error => Promise.reject(error))
         }
         return res.json()
       })
       .then(this.setBookmarks)
-      .catch(error => this.setState({ error }))
+      .catch(error => {
+        console.error(error)
+        this.setState({ error })
+      })
+    }
+  
+
+  updateBookmark = updatedBookmark => {
+    this.setState({
+      bookmarks: this.state.bookmarks.map(bm =>
+        (bm.id !== updatedBookmark.id) ? bm : updatedBookmark
+      )
+    })
   }
 
   render() {
@@ -83,6 +71,7 @@ class App extends Component {
       bookmarks: this.state.bookmarks,
       addBookmark: this.addBookmark,
       deleteBookmark: this.deleteBookmark,
+      updateBookmark: this.updateBookmark,
     }
     return (
       <main className='App'>
@@ -91,17 +80,20 @@ class App extends Component {
           <Nav />
           <div className='content' aria-live='polite'>
             <Route
-              path='/add-bookmark'
-              component={AddBookmark}
-            />
-            <Route
               exact
               path='/'
               component={BookmarkList}
             />
+            <Route
+              path='/add-bookmark'
+              component={AddBookmark}
+            />
+            <Route
+              path='/edit/:bookmarkId'
+              component={EditBookmark}
+            />
           </div>
         </BookmarksContext.Provider>
-        <Rating />
       </main>
     );
   }
